770939cec5a907b695ab68c630e6b3f45fc9470f
William Huang <shsidforever@gmail.com>
2017-12-29 14:33:41 +0800

    code clean up. Using render instead of loader

--- music/views.py
+++ music/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Album
 # Create your views here.
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
     context = {
         'all_albums': all_albums,
     }
-    return HttpResponse(template.render(context, request))
+    # render has http response built in
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
     return HttpResponse('<h1>ALBUM ID</h1>')
