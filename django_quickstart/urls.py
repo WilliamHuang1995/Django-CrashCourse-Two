b75fdb871e8807b700ba6e8549606e4a43fe66f1
William Huang <shsidforever@gmail.com>
2017-12-29 14:57:36 +0800

    added layout file

--- django_quickstart/urls.py
+++ django_quickstart/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('music.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
 ]
