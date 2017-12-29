f065081a119030086a47e618627d3dba1f915caa
William Huang <shsidforever@gmail.com>
2017-12-29 15:16:52 +0800

    dyanmic link

--- music/urls.py
+++ music/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/id/
+    # /music/id/ or /music/<album_id>
     url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
 ]
